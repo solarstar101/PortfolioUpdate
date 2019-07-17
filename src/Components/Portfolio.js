e526473fb0d6ad8e634268b7dc8dc74f258eac88
solarstar101 <45614244+solarstar101@users.noreply.github.com>
2019-07-17 15:28:45 -0500

    update

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -16,9 +16,9 @@ class Portfolio extends Component {
                      <p>{projects.category}</p>
                   </div>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
             </a>
           </div>
+
         </div>
       })
     }
